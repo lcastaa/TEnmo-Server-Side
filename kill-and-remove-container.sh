a34417a2c73ee87fdb1083cb6c23664b86c7ee63
Luis A Castaneda <lcastaa.aql@gmail.com>
2023-03-09 07:09:15 -0800

    update2

--- kill-and-remove-container.sh
+++ kill-and-remove-container.sh
@@ -6,12 +6,17 @@ container_name="$1"
 # Get the container ID based on the container name
 container_id="$(docker ps -aqf "name=$container_name")"
 
-# Check if the container is running
+# Check if the container ID is not empty
 if [ ! -z "$container_id" ]; then
-    # Stop the container
-    docker stop "$container_id"
-fi
+    # Check if the container is running
+    if docker ps | grep "$container_id" &> /dev/null; then
+        # Stop the container
+        docker stop "$container_id"
+    fi
 
-# Remove the container
-docker rm -f "$container_id"
+    # Remove the container
+    docker rm "$container_id"
+else
+    echo "Container not found: $container_name"
+fi
 
