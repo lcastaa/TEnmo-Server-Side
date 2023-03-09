d82e6d91ae63794089db16cc12ea928a076a48bc
Luis A Castaneda <lcastaa.aql@gmail.com>
2023-03-09 07:01:18 -0800

    update2

--- kill-and-remove-container.sh
+++ kill-and-remove-container.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-# Get the container ID as an argument
-container_id="$1"
+# Get the container name as an argument
+container_name="$1"
+
+# Get the container ID based on the container name
+container_id="$(docker ps -aqf "name=$container_name")"
 
 # Check if the container is running
-if docker ps | grep "$container_id" &> /dev/null; then
+if [ ! -z "$container_id" ]; then
     # Stop the container
     docker stop "$container_id"
 fi
 
 # Remove the container
 docker rm "$container_id"
+
