76a36b3be96ab3761bdf6730078f3f50b13696d2
Luis A Castaneda <lcastaa.aql@gmail.com>
2023-03-09 12:00:16 -0800

    Added new controller via Pipeline

--- src/main/java/com/techelevator/tenmo/controller/HelloController.java
+++ src/main/java/com/techelevator/tenmo/controller/HelloController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/hello")
 @PreAuthorize("isAuthenticated()")
+@RequestMapping(path = "/test")
 public class HelloController {
 
     @GetMapping
